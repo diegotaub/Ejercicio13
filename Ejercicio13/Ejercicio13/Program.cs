b105721be3832992e0677d8758bce603790f0c05
diego.taub <diego.taub@accenture.com>
2021-03-21 20:52:45 -0300

    Ejercicio13

---
+++ Ejercicio13/Ejercicio13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la primera frase: ");
+            string frase = Console.ReadLine();
+            Console.Write("Ingrese la segunda frase: ");
+            if(frase.Length == Console.ReadLine().Length)
+            {
+                Console.Write("Las frases tienen la misma longitud");
+            }
+            else
+            {
+                Console.Write("Las frases tienen diferente longitud");
+            }
+            Console.ReadKey();
+        }
+    }
+}
